db2e11cd427e4a411f03d8fec4a7ed807fdde87c
Ayaz <ayazzali@mail.ru>
2018-03-27 22:22:11 +0300

    works: generate - encode - vk -  decode

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
     - node t.js 
 
 может надо будет:
-- npm install keypair
+- //npm install keypair
 - npm install -g browserify
+- //npm install -g jsrsasign  
+- npm install -g cryptico  
 
 можете писать текст в консоле и он придёт в вкМарата)
--- t.js
+++ t.js
@@ -1,11 +1,57 @@
+//import { log } from "util";
+
 'use strict';
-let fetch = require("node-fetch") //todo
+const fetch = require("node-fetch") //todo
 // import {vk} from "vk" //todo
 
-// var keypair = require('keypair');
-// var pair = keypair();
-console.log("init...");
-var ACCESS_TOKEN = "77b611c7db3ce9c89112a91894e2a2fa9daf00bc80d3345f757a047e638ec8d18a4d56985f13f5fd3842f"
+//var keypair = require('keypair');
+//var __pair = keypair();
+// var rs = require("jsrsasign");
+// //var rsu = require("jsrsasign-util"); // for file I/O
+// var kp = rs.KEYUTIL.generateKeypair("RSA", 2048);
+// var prvPEM = rs.KEYUTIL.getPEM(kp.prvKeyObj, "PKCS8PRV", "password");
+// //rsu.saveFile("foo.key", prvPEM);
+// console.log(prvPEM)
+//
+//
+var cryptico = require("cryptico");
+var PassPhrase = "The Moon is a Harsh Mistress.";//what is it??????????
+// The length of the RSA key, in bits. 
+var Bits = 1024;
+var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
+var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);
+var PlainText = "Matt"
+
+//var EncryptionResult = cryptico.encrypt(PlainText, MattsPublicKeyString);
+//console.log(EncryptionResult)
+console.log(MattsPublicKeyString)
+
+//var DecryptionResult = cryptico.decrypt(CipherText, MattsRSAkey);
+//console.log(DecryptionResult.plaintext)
+// return;
+
+// var algName = "RSA";
+// var keyObj = kp.pubKeyObj//rs.KEYUTIL.getKey(keyStr);
+// let encHex = rs.KJUR.crypto.Cipher.encrypt("aaaaaaaaaa");
+// console.log(encHex)
+// return ;
+
+// var prv =kp.prvKeyObj//= KEYUTIL.getKey("-----BEGIN ENCRYPTED PRIVATE KEY...", "pass");
+// // generate Signature object
+// var sig = new rs.KJUR.crypto.Signature({"alg": "SHA256withRSA"});
+// // set private key for sign
+// sig.init(prv);
+// // update data
+// sig.updateString("aaa  objectaaa  aaa");
+// // calculate signature
+// console.log(sig)
+// var sigHex = sig.sign();
+// console.log("--")
+// console.log(sigHex)
+// return ;
+
+ console.log("init...");
+var ACCESS_TOKEN = "9778b47928c7c593dd83b41b84172d96ee0910047472aef9d90e22d581e01c4b5df7962d4aff1f5f31717"
 
 //updates:
 longPoll()
@@ -16,41 +62,44 @@ stdin.addListener("data", function (d) {
     // note:  d is an object, and when converted to a string it will
     // end with a linefeed.  so we (rather crudely) account for that  
     // with toString() and then trim() 
-    let maratId=75332891;
-    //console.trace(d)
-    Send(d.toString().trim(),maratId)
+    let maratId = 75332891;
+    let text = d.toString().trim();
+    text = encodeURI(text)
+    text = cryptico.encrypt(text, MattsPublicKeyString).cipher
+    //text = btoa(text)// base64
+    console.log(text)
+    Send(text, maratId)
 });
 
-function Send(text="ghbftn", to=75332891, token) {
-    fetch("https://api.vk.com/method/messages.send?user_id="+to+
-        "&message="+text+"&access_token=" + ACCESS_TOKEN + "&v=5.73"
+function Send(text = "default text", to = 75332891, token) {
+    fetch("https://api.vk.com/method/messages.send?user_id=" + to +
+        "&message=" + text + "&access_token=" + ACCESS_TOKEN + "&v=5.73"
         , { method: 'GET', mode: 'cors' })
         .then(function (response) {
             //console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
-            console.log("send: "+response.status); // 200
+            console.log("Send status: " + response.status); // 200
             //console.log(response);
             return response.json();
         })
         .then(function (cb) {
             console.log("Send response:")
             console.log(cb);
-        }).catch(console.log);
+        }).catch(console.error);
 
 }
 function longPoll(text, to, token) {//todo ACCESS_TOKEN (..)
     function upd(server, key, ts) {
         return fetch("https://" + server + "?act=a_check&key=" + key + "&ts=" + ts + "&wait=25&mode=2&version=2")
-            .then((rawUpdate) => {
-                //console.log(rawUpdate)
-                return rawUpdate.json()
-            }).then((update) => {
-                //console.log(update)
-                return update
-            }).catch(console.log);
+        .then((rawUpdate) => {
+            //console.log(rawUpdate)
+            return rawUpdate.json()
+        }).then((update) => {
+            //console.log(update)
+            return update
+        }).catch(console.error);
     }
 
-    fetch("https://api.vk.com/method/messages.getLongPollServer?access_token="
-        + ACCESS_TOKEN + "&v=5.73")
+    fetch("https://api.vk.com/method/messages.getLongPollServer?access_token=" + ACCESS_TOKEN + "&v=5.73")
         .then(function (response) {
             console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
             console.log(response.status); // 200
@@ -58,7 +107,7 @@ function longPoll(text, to, token) {//todo ACCESS_TOKEN (..)
         })
         .then(function (lp) {
             var r = lp.response;
-            (async () => {
+            (async () => {// по другому не умею запускать await fetch() --ниже 
                 var ts = r.ts
                 while (true) {
                     try {
@@ -69,13 +118,19 @@ function longPoll(text, to, token) {//todo ACCESS_TOKEN (..)
                             result.updates.forEach(element => {
                                 let action_code = element[0]  // запись в переменную кода события
                                 if (action_code == 4)  // проверка кода события
-                                    console.log(element[5]) 
+                                {
+                                    let text = element[5].split(" ").join("+")/// ибо вк видимо энкодит  //atob(element[5])
+                                    console.log(text)
+                                    var DecryptionResult = cryptico.decrypt(text, MattsRSAkey);
+                                    let plainText = decodeURI(DecryptionResult.plaintext)
+                                    console.log("decripted: " + plainText)
+                                }
                             });
-                    } catch (e) {console.log(e)}
+                    } catch (e) { console.error(e) }
                 }
             })();
         })
-        .catch(console.log);
+        .catch(console.error);
 
 
 }
